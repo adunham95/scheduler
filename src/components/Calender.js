4c6166978c8c4f3b8e0ffe3657be7cd5a80916eb
Adrian <adunham95@gmail.cooom>
2021-06-01 08:44:37 -0400

    Added calender view

--- src/components/Calender.js
+++ src/components/Calender.js
@@ -1,38 +1,51 @@
 import React from 'react';
 
-function generateDataSet(i) {
-  return { array: i };
-}
+const arr = [
+  [{ array: 0, width: '20%' }, { array: 1, width: '10%' }, { array: 2, width: '25%' }, { array: 3, width: '15%' }, { array: 4, width: '10%' }, { array: 5, width: '20%' }],
+  [{ array: 0, width: '15%' }, { array: 1, width: '10%' }, { array: 2, width: '30%' }, { array: 3, width: '20%' }, { array: 4, width: '10%' }, { array: 5, width: '25%' }],
+  [{ array: 0, width: '15%' }, { array: 1, width: '10%' }, { array: 2, width: '30%' }, { array: 3, width: '20%' }, { array: 4, width: '10%' }, { array: 5, width: '25%' }],
+  [{ array: 0, width: '15%' }, { array: 1, width: '10%' }, { array: 2, width: '30%' }, { array: 3, width: '20%' }, { array: 4, width: '10%' }, { array: 5, width: '25%' }],
+];
 
 export const Calender = () => {
   const rows = 5;
-  const columns = 5;
-  const arr = [...Array(rows)].map(() => [...Array(columns)].map((_, i) => (generateDataSet(i))));
+
+  const weekdays = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri'];
+
+  const blockStyle = {
+    backgroundColor: 'purple', margin: '5px', padding: '2px', borderRadius: '7px', color: 'white',
+  };
+  const rowStyle = {
+    display: 'flex',
+    minHeight: '2em',
+    backgroundColor: 'grey',
+  };
 
   function generateTable() {
-    console.log(arr);
-    arr.forEach((r, i) => {
-      r.forEach((c, j) => {
-        console.log(c);
-      });
-    });
-
-    return <div />;
+    return (
+      <>
+        {
+            [...Array(rows)].map((_, i) => (
+              <div style={rowStyle}>
+                <div style={{
+                  minWidth: '3em', textAlign: 'left', paddingLeft: '5px', marginRight: '5px', borderRight: '1px solid black',
+                }}
+                >
+                  {weekdays[i]}
+                </div>
+                {
+                    typeof arr[i] !== 'undefined' ? arr[i].map((c, j) => <div style={{ ...blockStyle, width: c.width }}>{c.array}</div>) : <div />
+                }
+              </div>
+            ))
+        }
+      </>
+    );
   }
 
   return (
-    <div style={{
-      display: 'grid', rowGap: '5px', columnGap: '5px', columnCount: columns, gridRow: rows,
-    }}
-    >
-      <div style={{ backgroundColor: 'purple', height: '10px', width: '10px' }} />
-      <div style={{ backgroundColor: 'purple', height: '10px', width: '10px' }} />
-      <div style={{ backgroundColor: 'purple', height: '10px', width: '10px' }} />
-      <div style={{ backgroundColor: 'purple', height: '10px', width: '10px' }} />
-      <div style={{ backgroundColor: 'purple', height: '10px', width: '10px' }} />
-      <div style={{ backgroundColor: 'purple', height: '10px', width: '10px' }} />
-      <div style={{ backgroundColor: 'purple', height: '10px', width: '10px' }} />
-      <div style={{ backgroundColor: 'purple', height: '10px', width: '10px' }} />
+    <div>
+      {generateTable()}
     </div>
   );
 };
