80590d6d88c135b51aac9c628a540ce893ba3c0c
Adrian <adunham95@gmail.cooom>
2021-06-14 15:07:47 -0400

    Added header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Calender } from './components/Calender/Calender';
+import { Header } from './components/Header/Header';
 import { ModalProvider } from './components/Modal/ModalContext';
 import { NewBlock } from './components/NewBlock/NewBlock';
 import { NewBlockButton } from './components/NewBlock/NewBlockButton';
@@ -8,7 +9,12 @@ function App() {
   return (
     <ModalProvider>
       <div className="App">
-        <NewBlockButton />
+        <Header />
+        <main>
+          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+            <NewBlockButton />
+          </div>
+        </main>
       </div>
     </ModalProvider>
   );
