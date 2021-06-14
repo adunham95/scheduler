6e7f203688dcb58b33ff60b86bfc4593a42b43ed
Adrian <adunham95@gmail.cooom>
2021-06-14 14:39:33 -0400

    MOved colors to own function

--- src/components/NewBlock/NewBlock.js
+++ src/components/NewBlock/NewBlock.js
@@ -1,17 +1,5 @@
 import React, { useState } from 'react';
-
-const colors = [
-  { name: 'Red', color: '#f44336' },
-  { name: 'Pink', color: '#E91E63' },
-  { name: 'Purple', color: '#673ab7' },
-  { name: 'Blue', color: '#0085ca' },
-  { name: 'Teal', color: '#00796B' },
-  { name: 'Green', color: '#009688' },
-  { name: 'Yellow', color: '#f1dd2f' },
-  { name: 'Orange', color: '#FF8201' },
-  { name: 'Brown', color: '#5D4037' },
-  { name: 'Smokey', color: '#58595C' },
-];
+import { colors } from '../../utils/data';
 
 export const NewBlock = () => {
   const [name, setName] = useState('');
@@ -35,50 +23,99 @@ export const NewBlock = () => {
 
   return (
     <div>
-      <div>
-        <div style={{
-          backgroundColor: selectedColor, padding: '10px', borderRadius: '7px', color: 'white',
-        }}
-        >
-          <p>{name}</p>
+      <form
+        onSubmit={saveBlock}
+        className="md:grid md:grid-cols-3 md:gap-6 max-w-lgw"
+      >
+        <h1>Create New Block</h1>
+        <div className="col-span-6 sm:col-span-3">
+          <label
+            htmlFor="first_name"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Block Name
+          </label>
+          <input
+            type="text"
+            name="block_name"
+            id="block_name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+          />
+
         </div>
-      </div>
-      <form onSubmit={saveBlock}>
-        <input
-          placeholder="Block Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <fieldset>
+        {/* <label className="block">
+          <span className="text-gray-700">Block Name</span>
           <input
-            type="number"
-            value={timeHours}
-            onChange={(e) => setTimeHours(e.target.value)}
+            type="text"
+            placeholder="Lunch"
+            className="mt-0 block w-full px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-black"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
+        </label>
+        <label className="block">
+          <span className="text-gray-700">Block Name</span>
           <input
-            type="number"
-            value={timeMins}
-            onChange={(e) => setTimeMins(e.target.value)}
+            type="text"
+            placeholder="Lunch"
+            className="mt-0 block w-full px-0.5 border-0 border-b-2 border-gray-200 focus:ring-0 focus:border-black"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
-        </fieldset>
-        <fieldset>
+        </label> */}
+        <fieldset className="col-span-6 sm:col-span-3">
+          <label className="block text-sm font-medium text-gray-700">Color</label>
           <div>
             {
             colors.map((c) => (
-              <button
-                type="button"
-                aria-label={`Color ${c.name}`}
-                onClick={() => setSelectedColor(c.color)}
-                style={{
-                  backgroundColor: c.color, height: '40px', width: '40px', margin: '5px',
-                }}
+              <ColorBlock
+                colorData={c}
+                selectedColor={selectedColor}
+                setColor={(color) => setSelectedColor(color)}
               />
             ))
         }
           </div>
         </fieldset>
-        <button>Submit</button>
+        <div className="px-4 py-3 text-right sm:px-6">
+          <button
+            type="submit"
+            className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Save
+          </button>
+        </div>
       </form>
     </div>
   );
 };
+
+function ColorBlock({ colorData: { name, color }, selectedColor, setColor }) {
+  function setStyle() {
+    if (color === selectedColor) {
+      return {
+        padding: '1px', borderColor: color, outlineColor: 'transparent',
+
+      };
+    }
+    return {
+      padding: '1px', borderColor: 'transparent', outlineColor: 'transparent',
+    };
+  }
+  return (
+    <button
+      type="button"
+      aria-label={`Color ${name}`}
+      onClick={() => setColor(color)}
+      className="rounded-full w-8 h-8 m-1 border-2 border-solid outline-none"
+      style={setStyle()}
+    >
+      <span style={{
+        backgroundColor: color, height: '100%', width: '100%', borderRadius: '100%', display: 'block',
+      }}
+      />
+    </button>
+  );
+}
