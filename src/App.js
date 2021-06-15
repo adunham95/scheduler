ef986445fe7dad0b5076bd79d4f1f39e2dcf9780
Adrian <adunham95@gmail.cooom>
2021-06-15 08:13:24 -0400

    Working on routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
 import { BlockCard } from './components/Block/BlockCard';
 import { BlockList } from './components/Block/BlockList';
 import { Calender } from './components/Calender/Calender';
@@ -11,16 +17,29 @@ function App() {
   return (
     <ModalProvider>
       <div className="App">
-        <Header />
-        <main>
-          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-            <NewBlockButton />
-            {/* <BlockCard /> */}
-            <BlockList />
-          </div>
-        </main>
+        <Router>
+          <Header />
+          <main>
+            <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+              <Switch>
+                <Route path="/">
+                  <Home />
+                </Route>
+              </Switch>
+            </div>
+          </main>
+        </Router>
       </div>
     </ModalProvider>
+  );
+}
+function Home() {
+  return (
+    <>
+      <NewBlockButton />
+      {/* <BlockCard /> */}
+      <BlockList />
+    </>
   );
 }
 
